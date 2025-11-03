66db5f850ad53f610a309d3d9aa6c9b34ba96732
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-11-03 16:00:26 +0100

    Split functionality into FastLoot and AutoVendor modules

--- AutoVendor.lua
+++ AutoVendor.lua
@@ -21,29 +21,3 @@ end
 local eventFrame = CreateFrame("Frame")
 eventFrame:RegisterEvent("MERCHANT_SHOW")
 eventFrame:SetScript("OnEvent", processMerchantWindow)
-
-
-
--- Makes looting instant with no delay when auto loot is enabled
-
-local lastLootTime = 0
-local lootDelay = 0.025
-
-local function lootAllSlots()
-    local autoLootEnabled = GetCVarBool("autoLootDefault")
-    local shiftPressed = IsModifiedClick("AUTOLOOTTOGGLE")
-    
-    if autoLootEnabled ~= shiftPressed then
-        if (GetTime() - lastLootTime) >= lootDelay then
-            for slotIndex = GetNumLootItems(), 1, -1 do
-                LootSlot(slotIndex)
-            end
-            
-            lastLootTime = GetTime()
-        end
-    end
-end
-
-local eventFrame = CreateFrame("Frame")
-eventFrame:RegisterEvent("LOOT_READY")
-eventFrame:SetScript("OnEvent", lootAllSlots)
